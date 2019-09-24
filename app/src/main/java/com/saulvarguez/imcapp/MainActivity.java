1f4ba657177df05e03a7236dd31aac0c3c54de15
Saul Stevens Varguez Velazquez <e18470427@itcampeche.edu.mx>
2019-09-23 23:08:44 -0500

    Proyecto ImcApp, que calcula el Índice de Masa Corporal (IMC).

---
+++ app/src/main/java/com/saulvarguez/imcapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.saulvarguez.imcapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if(fragment == null) {
+            fragment = new ImcFragment();
+            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
+
+        }
+
+    }
+}
